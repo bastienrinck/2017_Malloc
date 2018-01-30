264a506523a619c4dbf4fd213ffc876dedfd0310
Thibaut Cornolti <thibaut@cornolti.eu>
2018-01-30 18:04:57 +0100

    malloc

--- main.c
+++ main.c
@@ -3,8 +3,19 @@
 
 int main()
 {
-	char *str = malloc(sizeof(char) * 12);
-
+	char *str;
+	
+	write(1, "\nfirst malloc\n", 15);
+	str = malloc(sizeof(char) * 12);
+//	printf("%x\n", str);
+	write(1, "\nfirst free\n", 13);
+	free(str);
+	write(1, "\nsecond malloc\n", 16);
+	str = malloc(sizeof(char) * 3);
+//	printf("%x\n", str);
+	write(1, "\nthird malloc\n", 15);
+	str = malloc(sizeof(char) * 11);
+	write(1, "\nprintf\n", 9);
 	printf("%x\n", str);
 	return 0;
 }
--- malloc.c
+++ malloc.c
@@ -14,6 +14,7 @@ metadata_t *freed = NULL;
 
 static void *init_heap(size_t i)
 {
+	write(1, "init\n", 5);
 	if ((allocated = sbrk(i + HEADER)) == (void *)-1)
 		return (NULL);
 	allocated->size = i;
@@ -28,6 +29,7 @@ static void *resize_heap(size_t i)
 	metadata_t *temp;
 	metadata_t *iter;
 
+	write(1, "resize\n", 8);
 	if ((temp = sbrk(i + HEADER)) == (void *)-1)
 		return (NULL);
 	iter = allocated;
@@ -45,23 +47,33 @@ static void *realloc_freed(metadata_t *pMetadata)
 {
 	metadata_t *temp = allocated;
 
-	pMetadata->next->prev = pMetadata->prev;
-	pMetadata->prev->next = pMetadata->next;
-	pMetadata->ptr = pMetadata + HEADER;
+	write(1, "realloc\n", 9);
+	if (pMetadata->next)
+		pMetadata->next->prev = pMetadata->prev;
+	write(1, "realloc1\n", 10);
+	if (pMetadata->prev)
+		pMetadata->prev->next = pMetadata->next;
+	else
+		freed = pMetadata->next;
+	write(1, "realloc2\n", 10);
 	while (temp->next && temp->next < pMetadata)
 		temp = temp->next;
+	write(1, "realloc3\n", 10);
 	if (temp != allocated) {
 		pMetadata->next = temp->next;
 		pMetadata->prev = temp;
 		if (pMetadata->next)
 			pMetadata->next->prev = pMetadata;
 		temp->next = pMetadata;
+		write(1, "realloc4\n", 10);
 	} else {
 		pMetadata->next = temp;
 		temp->prev = pMetadata;
 		pMetadata->prev = NULL;
 		allocated = pMetadata;
+		write(1, "realloc5\n", 10);
 	}
+	write(1, "realloc6\n", 10);
 	return (pMetadata + HEADER);
 }
 
@@ -71,8 +83,9 @@ void *malloc(size_t size)
 
 	if (!allocated)
 		return (init_heap(size));
+	write(1, "while\n", 6);
 	while (temp) {
-		if (temp->size > size)
+		if (temp->size >= size)
 			return (realloc_freed(temp));
 		temp = temp->next;
 	}
